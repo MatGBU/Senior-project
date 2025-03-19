df31eae34a372e28dda2baaa22f156fcaa0ebfa9
konstan10 <kostya@bu.edu>
2025-03-19 15:33:12 -0400

    balls

--- paper-dashboard/src/views/Devices.js
+++ paper-dashboard/src/views/Devices.js
@@ -15,7 +15,7 @@ function Devices() {
   // Function to handle "Turn On" button click
   const handleTurnOn = (input) => {
     axios
-      .get(`https://04b9-128-197-28-191.ngrok-free.app/turn_on?input=${input}`, {
+      .get(`https://4c58-128-197-28-191.ngrok-free.app/turn_on?input=${input}`, {
         headers: new Headers({
           "ngrok-skip-browser-warning": "69420",  // Custom header
         }),
